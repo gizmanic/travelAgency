b49344dfd038ef75bfad171947e9577bae54cfa5
Dmitry <dmskvcv@gmail.com>
2018-12-30 00:58:36 -0500

    package.json changed to down vers

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,7 @@
+var $ = require('jquery');
+var person = require('./modules/Person');
+
+var john = new person("John Doe", "blue");
+
+john.greet();
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
 require('./gulp/tasks/gulpstyle');
 require('./gulp/tasks/gulpwatch');
 require('./gulp/tasks/sprites');
+require('./gulp/tasks/scripts');
