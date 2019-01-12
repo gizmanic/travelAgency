e87672adae2f7d22be510f7c90b4eff6aa010e4d
Dmitry <dmskvcv@gmail.com>
2019-01-12 00:59:25 -0500

    completed modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 //import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
+
 
 var mobilemenu = new MobileMenu();
 //var revealOnScroll = new RevealOnScroll();
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
